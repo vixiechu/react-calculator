f88b68aa27d78e806501786fc189f6b92bb8e749
Vix Morrigan <vixmorrigan@gmail.com>
2020-01-09 15:10:43 +0000

    part 1 complete

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Display from '../Display/Display';
+import Keypad from '../Keypad/Keypad';
 import './Calculator.css';
 
 class Calculator extends Component {
@@ -29,11 +30,19 @@ class Calculator extends Component {
   }
 
   render = () => {
-    const { displayValue } = this.state;
-
+    // unpack the component state by using Object Destructuring
+    const { displayValue, numbers, operators } = this.state;
+  
     return (
       <div className="calculator-container">
         <Display displayValue={displayValue} />
+        <Keypad
+          callOperator={this.callOperator}
+          numbers={numbers}
+          operators={operators}
+          setOperator={this.setOperator}
+          updateDisplay={this.updateDisplay}
+        />
       </div>
     );
   }
--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Calculator from './Calculator';
 import Display from '../Display/Display';
+import Keypad from '../Keypad/Keypad';
 
 describe('Calculator', () => {
   let wrapper;
 
   beforeEach(() => wrapper = shallow(<Calculator />));
+
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
@@ -14,7 +17,14 @@ describe('Calculator', () => {
 
   it('should render the Display Component', () => {
     expect(wrapper.containsMatchingElement(
-      <Display displayValue={wrapper.instance().state.displayValue} />
+      <Display displayValue={wrapper.instance().state.displayValue} />,
+      <Keypad
+      callOperator={wrapper.instance().callOperator}
+      numbers={wrapper.instance().state.numbers}
+      operators={wrapper.instance().state.operators}
+      setOperator={wrapper.instance().setOperator}
+      updateDisplay={wrapper.instance().updateDisplay}
+    />
     )).toEqual(true);
   });
 });
