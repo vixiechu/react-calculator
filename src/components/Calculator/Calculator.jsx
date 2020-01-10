921af7d8c787e19dd466d6e5673e4ff845a8b049
Vix Morrigan <vixmorrigan@gmail.com>
2020-01-10 09:52:13 +0000

    Calculator functional

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -13,22 +13,60 @@ class Calculator extends Component {
   }
 
   callOperator = () => {
-    console.log('call operation');
-  }
+    let { displayValue, selectedOperator, storedValue } = this.state;
+    const updateStoredValue = displayValue;
 
-  setOperator = () => {
-    console.log('set operation');
+    displayValue = parseInt(displayValue, 10);
+    storedValue = parseInt(storedValue, 10);
+
+    switch (selectedOperator) {
+      case '+':
+        displayValue = storedValue + displayValue;
+        break;
+      case '-':
+        displayValue = storedValue - displayValue;
+        break;
+      case 'x':
+        displayValue = storedValue * displayValue;
+        break;
+      case '/':
+        displayValue = storedValue / displayValue;
+        break;
+      default:
+        displayValue = '0';
+    }
+
+    displayValue = displayValue.toString();
+    selectedOperator = '';
+    if (displayValue === 'NaN' || displayValue === 'Infinity') displayValue = '0';
+
+    this.setState({ displayValue, selectedOperator, storedValue: updateStoredValue });
+  };
+
+  setOperator = value => {
+    let { displayValue, selectedOperator, storedValue } = this.state;
+
+    if (selectedOperator === '') {
+      storedValue = displayValue;
+      displayValue = '0';
+      selectedOperator = value;
+    } else {
+      selectedOperator = value;
+    }
+
+    this.setState({ displayValue, selectedOperator, storedValue });
   }
 
   updateDisplay = value => {
     let { displayValue } = this.state;
+
     if (value === '.' && displayValue.includes('.')) value = '';
 
     if (value === 'ce') {
       displayValue = displayValue.substr(0, displayValue.length - 1);
       if (displayValue === '') displayValue = '0';
     } else {
-      displayValue === '0' ? displayValue = value : displayValue += value;
+      displayValue === '0' ? (displayValue = value) : (displayValue += value);
     }
 
     this.setState({ displayValue });
